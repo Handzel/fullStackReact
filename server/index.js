e0369a58046f57b89c8da1e40f1e06e429e2d41d
Handzel <handzelewicz.dev@gmail.com>
2018-07-13 15:30:35 +0200

    mongoDB & mongoose setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 // CommonJS
 const express = require('express');
+const mongoose = require('mongoose');
 // only 'require' without const declaration i.e passportConfig
 // because /services/passport.js does not return anything
 require('./services/passport')
+const keys = require('./config/keys');
+
 
 //const authRoutes = require('./routes/auth');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
