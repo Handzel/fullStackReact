fac778ce40799ffe64bd631ed1ef37bb715a6301
Handzel <handzelewicz.dev@gmail.com>
2018-07-13 11:28:33 +0200

    Passport google oauth2.0 setup -> retrieving google access token

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
-// CommonJs
+// CommonJS
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'Hello'});
-});
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
+    }
+));
 
-app.listen(5004);
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', 
+    {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google')
+);
+
+const port = process.env.PORT || 5004;
+app.listen(port, () => console.log(`Server started on ${port}`));
