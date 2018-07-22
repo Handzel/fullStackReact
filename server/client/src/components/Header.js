bd99ffad8c6ee14febc488f4b70ab04580867f5e
Handzel <handzelewicz.dev@gmail.com>
2018-07-22 21:38:13 +0200

    Landing.js component + React Router <Link> tags instead of <a> for logo

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -22,7 +23,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="brand-logo center">Emaily</a>
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/' } 
+                        className="brand-logo left"
+                    >
+                        Emaily
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
