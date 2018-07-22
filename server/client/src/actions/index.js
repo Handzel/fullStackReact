43a70ab8c4b8128e812b8a4521a62e6c8736224b
Handzel <handzelewicz.dev@gmail.com>
2018-07-22 10:30:48 +0200

    fetchUser action added on component load + further refactor of actionCreator

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -5,10 +5,18 @@ import { FETCH_USER } from './types';
 // Redux-thunk middleware applied to store (client/index.js) checks wheter 
 // action creator returns object/action or function/ as below
 // if it returns function than redux-thunk inserts dispatch()
-const fetchUser = () => {
-    return function(dispatch) {
-        axios
-            .get('/api/current_user')
-            .then(res => dispatch({ type: FETCH_USER, payload: res }));
-    };
+
+// export const fetchUser = () => {
+//     return function(dispatch) {
+//         axios
+//             .get('/api/current_user')
+//             .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//     };
+// };
+
+// Refactored version - if arrow function contains only single expression
+// we can drop {} and 'return' keyword and more ...
+export const fetchUser = () => async dispatch => {
+        const res = await axios.get('/api/current_user')
+        dispatch({ type: FETCH_USER, payload: res });
 };
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,7 @@
 // Records whether or not user is logged in
  
 export default function(state = {}, action) {
+    console.log(action)
     switch (action.type) {
         default:
             return state;
