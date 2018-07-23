020b183f956e651050e0a7049b2b42e3e797ac35
Handzel <handzelewicz.dev@gmail.com>
2018-07-23 21:54:45 +0200

    Payments fixes

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -3,14 +3,18 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
     render() {
-        debugger;
-
         return (
             <StripeCheckout 
+                name="Emaily"
+                description="$5 for 5 email credits"
                 amount={500} // value in US cents 0.01$ - by default its USD
                 token={token => console.log(token)} // cb - representation of charge
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">
+                    Add credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
