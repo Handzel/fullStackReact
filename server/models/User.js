7fa5907a69bc6e55c3b197cb07e4aead96858bab
Handzel <handzelewicz.dev@gmail.com>
2018-07-25 22:54:09 +0200

    Adding Credits to User upon confirmation from stripe API + requiring Authentication on /api/stripe

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String
+    googleId: String,
+    credits: {
+        default: 0,
+        type: Number
+    }
 }); 
 
 //module.exports = User = mongoose.model('users', userSchema);
