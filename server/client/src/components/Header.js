160348b7d44723bb56cae55f82b21487fb3420b4
Handzel <handzelewicz.dev@gmail.com>
2018-07-29 15:12:50 +0200

    Displaying credit Quantity in Header upon purchase/billing with stripe API

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -17,6 +17,7 @@ class Header extends Component {
             default:
                 return [
                     <li key="1"><Payments /></li>,
+                    <li key="3"><a>Credits: {this.props.auth.credits}</a></li>,
                     <li key="2"><a href="/api/logout">Logout</a></li>
                 ];
         }
