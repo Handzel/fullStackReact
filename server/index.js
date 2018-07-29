69fb9b291227bd77297c2de60d9529ef1c82e3c4
Handzel <handzelewicz.dev@gmail.com>
2018-07-29 15:41:16 +0200

    configuration for routing on Production -> server/index.js

--- server/index.js
+++ server/index.js
@@ -33,5 +33,20 @@ app.use(passport.session());
 require('./routes/auth')(app);
 require('./routes/billing')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // ORDER is relevant!
+    
+    // FIRST. Express will serve up prod assets
+    // like our main.js file or main.css file
+    app.use(express.static('client/build'));
+
+    // IF PREVIOUS ATTEMPT FAILED. Express will serve up the index.html file
+    // if route is not recognized
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5004;
 app.listen(port, () => console.log(`Server started on ${port}`));
